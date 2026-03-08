3b4ad93e9d81ba929a0ba87d4b8361e8720ff3fe
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2026-03-08 20:47:24 +0530

    feat: Implement core authentication features and document API response, error, and creation standards.

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -16,9 +16,8 @@ const setAuthCookies = (
     httpOnly: true,
     secure: isProduction,
     sameSite: (isProduction ? 'none' : 'lax') as any,
-     domain: isProduction ? ".himanshutamoli.me" : undefined,
+    domain: isProduction ? '.himanshutamoli.me' : undefined,
   };
-  console.log("cookieOptions",cookieOptions);
   res.cookie('accessToken', accessToken, {
     ...cookieOptions,
     maxAge: 6 * 60 * 60 * 1000,
@@ -39,24 +38,40 @@ const setAuthCookies = (
 
 export const register = asyncHandler(async (req: Request, res: Response) => {
   const { user, accessToken, refreshToken } = await authService.register(req.body);
-  console.log("user",setAuthCookies);
   setAuthCookies(res, accessToken, refreshToken, user.role);
-  res
-    .status(201)
-    .json(new ApiResponse('Registered successfully', { ...user, accessToken, refreshToken }));
+  console.log('user', user);
+
+  res.status(201).json(
+    new ApiResponse('Registered successfully', {
+      _id: user._id,
+      name: user.name,
+      role: user.role,
+      email: user.email,
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    }),
+  );
 });
 
 export const login = asyncHandler(async (req: Request, res: Response) => {
   const { user, accessToken, refreshToken } = await authService.login(req.body);
-  console.log("user",setAuthCookies);
   if (!user) throw new Error('User not found');
   setAuthCookies(res, accessToken, refreshToken, user.role);
-  res.status(200).json(new ApiResponse('Login successful', { ...user, accessToken, refreshToken }));
+  console.log('user', user);
+  res.status(200).json(
+    new ApiResponse('Login successful', {
+      _id:user._id,
+      name: user.name,
+      role: user.role,
+      email: user.email,
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    }),
+  );
 });
 
 export const logout = asyncHandler(async (req: Request, res: Response) => {
   const { _id } = req.body;
-  console.log('Logging out user:', _id);
   await authService.logout(_id);
   res.clearCookie('accessToken');
   res.clearCookie('refreshToken');
